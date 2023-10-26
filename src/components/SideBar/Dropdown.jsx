65ddb60423c5e62529f80ba099af4610bf51c79b
baddestboy <giwasodiq7@gmail.com>
2023-10-26 15:28:57 +0100

    update

--- src/components/SideBar/Dropdown.jsx
+++ src/components/SideBar/Dropdown.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useAuth from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
-import "./Sidebar.css";
+import "./SideBar.css";
 
 const Dropdown = () => {
   const { token, handleLogOutUser } = useAuth();
