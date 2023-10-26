1dfce436e22fb4b87f18a2543514d34ea76f0cc6
baddestboy <giwasodiq7@gmail.com>
2023-10-26 16:13:53 +0100

    update

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://app-movie-server21.onrender.com",
 });
 export default axiosInstance;
